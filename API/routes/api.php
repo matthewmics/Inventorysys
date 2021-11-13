018d00d0d03d0492c98b5910b68bc851b9882bdb
Matthew Miclat <38750461+matthewmics@users.noreply.github.com>
2021-11-14 00:13:36 +0800

    Update API - Use sanctum instead of JWT

--- API/routes/api.php
+++ API/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BuildingController;
+use App\Models\Building;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,31 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-Route::group([
-
-    'middleware' => 'api',
-    'prefix' => 'auth'
-
-], function ($router) {
-
-    Route::post('login', 'App\Http\Controllers\AuthController@login');
-    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
-    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
-    Route::post('me', 'App\Http\Controllers\AuthController@me');
-
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
 
-Route::group([
 
-    'prefix' => 'user'
-
-], function ($router) {
-
-    Route::post('register', 'App\Http\Controllers\UserController@register');
-
+Route::get('/test', function (Request $request) {
+    return response('Hello World', 200)
+        ->header('Content-Type', 'text/plain');
 });
 
+Route::resource('buildings', BuildingController::class);
+// Route::get('/buildings', [BuildingController::class, 'index']);
+// Route::get('/buildings/{id}', [BuildingController::class, 'show']);
+// Route::post('/buildings', [BuildingController::class, 'store']);
