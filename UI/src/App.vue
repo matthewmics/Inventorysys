4e737f99df1de0bce4e3ed21b51e82a7196000ba
Matthew Miclat <38750461+matthewmics@users.noreply.github.com>
2021-11-15 00:22:26 +0800

    Update App.vue

--- UI/src/App.vue
+++ UI/src/App.vue
@@ -1,7 +1,34 @@
 <template>
-  <router-view></router-view>
+  <router-view v-if="!authLoading"></router-view>
+  <div v-else class="center-screen" style="flex-direction: column">
+    <md-progress-spinner
+      class="md-accent"
+      :md-diameter="30"
+      md-mode="indeterminate"
+    ></md-progress-spinner>
+    <h4>Loading...</h4>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapGetters, mapActions } from "vuex";
+export default {
+  created() {
+    this.currentUser();
+  },
+  computed: mapGetters(["authLoading"]),
+  methods: {
+    ...mapActions(["currentUser"]),
+  },
+};
 </script>
+
+<style>
+.center-screen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 100vh;
+}
+</style>
