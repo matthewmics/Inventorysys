80a0116b31bc72094816271e1e09812864e95478
Matthew Miclat <38750461+matthewmics@users.noreply.github.com>
2021-11-14 15:26:39 +0800

    Add interceptor for adding token to http requests

--- UI/src/agent.js
+++ UI/src/agent.js
@@ -3,6 +3,19 @@ import axios from "axios";
 axios.defaults.baseURL = process.env.VUE_APP_API_URL + "/api";
 const sleepDuration = process.env.VUE_APP_API_SLEEP_DURATION;
 
+axios.interceptors.request.use(
+  (config) => {
+      const token = window.localStorage.getItem("inventorysystem_sanctum_token");
+      if (token) {
+          config.headers.Authorization = "Bearer " + token;
+      }
+
+      return config;
+  },
+  (error) =>
+      Promise.reject(error)
+)
+
 const responseBody = (response) => response.data;
 
 const sleep = (ms) => (response) =>
