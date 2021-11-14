ba1c3074940be8490fff4a21522ce86588b7b425
Matthew Miclat <38750461+matthewmics@users.noreply.github.com>
2021-11-14 22:47:03 +0800

    Update Login.vue

--- UI/src/components/app/Login.vue
+++ UI/src/components/app/Login.vue
@@ -1,22 +1,57 @@
 <template>
-  <div style="width: 300px; margin: 0 auto; padding-top: 5em">
-    <h3>LOGIN</h3>
-    <md-field>
-      <label>Username</label>
-      <md-input v-model="initial"></md-input>
-    </md-field>
-    <md-field>
-      <label>Password</label>
-      <md-input v-model="initial" type="password"></md-input>
-    </md-field>
-
-    <md-button class="md-primary">Login</md-button>
-  </div>
+  <form
+    @submit="onSubmit"
+    style="width: 425px; margin: auto; padding-top: 10vh"
+    class="center"
+  >
+    <md-card>
+      <md-card-content>
+        <h3>LOGIN</h3>
+        <form action="">
+          <md-field>
+            <label>Email</label>
+            <md-input v-model="username"></md-input>
+          </md-field>
+          <md-field>
+            <label>Password</label>
+            <md-input v-model="password" type="password"></md-input>
+          </md-field>
+        </form>
+      </md-card-content>
+      <md-card-actions style="border: 0px">
+        <md-button :disabled="authLoading" type="submit" class="md-primary"
+          >Login</md-button
+        >
+      </md-card-actions>
+    </md-card>
+  </form>
 </template>
 
 <script>
-export default {};
+import { router } from "../../main";
+import { mapGetters, mapActions } from "vuex";
+export default {
+  data: () => {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  computed: mapGetters(["authLoading"]),
+  methods: {
+    ...mapActions(["login"]),
+    onSubmit(e) {
+      e.preventDefault();
+      this.login({
+        email: this.username,
+        password: this.password,
+      }).catch((err) => {
+        alert("Invalid email/password");
+      });
+    },
+  },
+};
 </script>
 
-<style scoped>
+<style>
 </style>
