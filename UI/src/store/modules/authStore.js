9ddce9b6404f8c8becc9ad9cd2f0543ae7f26fd3
Matthew Miclat <38750461+matthewmics@users.noreply.github.com>
2021-11-14 15:51:48 +0800

    Update authStore.js

--- UI/src/store/modules/authStore.js
+++ UI/src/store/modules/authStore.js
@@ -1,14 +1,35 @@
 import axios from "axios";
+import agent from "../../agent";
 
 const state = {
-  
+  user: {
+    id: 0,
+    name: "",
+    email: "",
+  },
+  loginLoading: false,
 };
 
-const getters = {};
+const getters = {
+  loggedInUser: (state) => state.user,
+};
 
-const actions = {};
+const actions = {
+  async login({ commit }, request) {
+    commit("setLoadingLoading", true);
+    try {
+      const response = await agent.User.login(request);
+    } catch (err) {
+      console.log(err);
+      alert("Invalid email/password");
+    }
+    commit("setLoadingLoading", false);
+  },
+};
 
-const mutations = {};
+const mutations = {
+  setLoadingLoading: (state, isLoading) => (state.loginLoading = isLoading),
+};
 
 export default {
   state,
